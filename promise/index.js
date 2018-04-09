0be2f957485954bbf5f77c1a6b3459fc06d37670
Shenfq <shenfq95@foxmail.com>
2018-04-10 00:14:19 +0800

    init

---
+++ promise/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const STATUS = {
+  PENDING: 'PENDING',
+  FULFILLED: 'FULFILLED',
+  REJECTED: 'REJECTED'
+}
+
+function Promise (func) {
+  
+  return this
+}
+
+
+
+
+export default Promise
