9333e0a7655745914488e62923e16d70c9320f8b
Shenfq <shenfq95@foxmail.com>
2018-05-28 23:27:37 +0800

    测试用例修改

--- promise/test/2.3.4.js
+++ promise/test/2.3.4.js
@@ -1,69 +1,68 @@
-"use strict";
+"use strict"
 
-var assert = require("assert");
-var testFulfilled = require("./helpers/testThreeCases").testFulfilled;
-var testRejected = require("./helpers/testThreeCases").testRejected;
+const assert = require("assert")
+const { testFulfilled, testRejected } = require('./testFunction')
 
-var dummy = { dummy: "dummy" }; // we fulfill or reject with this when we don't intend to test against it
+const dummy = { dummy: "dummy" } // we fulfill or reject with this when we don't intend to test against it
 
 describe("2.3.4: If `x` is not an object or function, fulfill `promise` with `x`", function () {
     function testValue(expectedValue, stringRepresentation, beforeEachHook, afterEachHook) {
         describe("The value is " + stringRepresentation, function () {
             if (typeof beforeEachHook === "function") {
-                beforeEach(beforeEachHook);
+                beforeEach(beforeEachHook)
             }
             if (typeof afterEachHook === "function") {
-                afterEach(afterEachHook);
+                afterEach(afterEachHook)
             }
 
             testFulfilled(dummy, function (promise1, done) {
                 var promise2 = promise1.then(function onFulfilled() {
-                    return expectedValue;
-                });
+                    return expectedValue
+                })
 
                 promise2.then(function onPromise2Fulfilled(actualValue) {
-                    assert.strictEqual(actualValue, expectedValue);
-                    done();
-                });
-            });
+                    assert.strictEqual(actualValue, expectedValue)
+                    done()
+                })
+            })
             testRejected(dummy, function (promise1, done) {
                 var promise2 = promise1.then(null, function onRejected() {
-                    return expectedValue;
-                });
+                    return expectedValue
+                })
 
                 promise2.then(function onPromise2Fulfilled(actualValue) {
-                    assert.strictEqual(actualValue, expectedValue);
-                    done();
-                });
-            });
-        });
+                    assert.strictEqual(actualValue, expectedValue)
+                    done()
+                })
+            })
+        })
     }
 
-    testValue(undefined, "`undefined`");
-    testValue(null, "`null`");
-    testValue(false, "`false`");
-    testValue(true, "`true`");
-    testValue(0, "`0`");
+    testValue(undefined, "`undefined`")
+    testValue(null, "`null`")
+    testValue(false, "`false`")
+    testValue(true, "`true`")
+    testValue(0, "`0`")
 
     testValue(
         true,
         "`true` with `Boolean.prototype` modified to have a `then` method",
         function () {
-            Boolean.prototype.then = function () {};
+            Boolean.prototype.then = function () {}
         },
         function () {
-            delete Boolean.prototype.then;
+            delete Boolean.prototype.then
         }
-    );
+    )
 
     testValue(
         1,
         "`1` with `Number.prototype` modified to have a `then` method",
         function () {
-            Number.prototype.then = function () {};
+            Number.prototype.then = function () {}
         },
         function () {
-            delete Number.prototype.then;
+            delete Number.prototype.then
         }
-    );
-});
+    )
+})
